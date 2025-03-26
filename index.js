82575408e33d550c8abee8e965f08c51cbaf6d7e
Cristina <kamuthucristina@gmail.com>
2025-03-26 06:58:04 +0300

    added and styled placeholder ans submit button

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-base_url ("http://localhost:3000/composers")
+let base_url = "http://localhost:3000/composers"
+
+document.addEventListener("DOMContentLoaded", () =>{
+const title = document.getElementById("title") 
+const input = document.getElementById("input") 
+const btn = document.getElementById("btn")
+
+btn.addEventListener("click", function () {
+    let newName = input.value.trim();
+if (newName !== "") {
+        title.textContent = `${newName}'s little musical world!`;
+    } else {
+        alert("Please enter a name!");
+    }
+});
+})
+
 
 
