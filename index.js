708525d8fb808283daad23657e5429c4a313c0ac
Cristina <kamuthucristina@gmail.com>
2025-03-27 05:31:42 +0300

    added submit button

--- index.js
+++ index.js
@@ -26,12 +26,65 @@ function displayDetails(composers) {
         <div class="content">
         </div>
         </section>
-        `;
+        <h2>Search & Play Music</h2>
+
+    <div class="container">
+    <label for="input"> 🎵</label>
+        <input type="text" id="searchBar" placeholder="Search for a song!...">
+        <button id="btn">submit</button>
+        <ul id="songList"></ul> 
+    </div>
+
+    <audio id="audioPlayer" controls>
+        <source id="audioSource" src="" type="audio/mp3">
+    </audio>
+        `
         classicalMusic.innerHTML += html;
-    });
+    })
 }
+
+// document.addEventListener("DOMContentLoaded",function () {
+//     const searchBar = document.getElementById("searchBar")
+//     const songList = document.getElementById("songList")
+//     const audioPlayer = document.getElementById("audioPLayer")
+//     const audioSource = document.getElementById ("audioSource")
+
+//     fetch("base_url")
+//                 .then(response => response.json())
+//                 .then(data => {
+//                     let songs = data.composers
+
+//                     function displaySongs(filter = "") {
+//                         songList.innerHTML = ""; 
+//                         songs.filter(song => song.title.toLowerCase().includes(filter.toLowerCase()))
+//                             .forEach(song => {
+//                                 const li = document.createElement("li");
+//                                 li.textContent = composer.music.title;
+//                                 li.dataset.src = composer.music.link;
+//                                 li.onclick = function () {
+//                                     audioSource.src = this.dataset.src;
+//                                     audioPlayer.load();
+//                                     audioPlayer.play();
+//                                 };
+//                                 songList.appendChild(li);
+//                             });
+//                     }
+
+//                      searchBar.addEventListener("", function () {
+//                         displaySongs(searchBar.value);
+//                     });
+
+//                     displaySongs()
+//                 })
+//                 .catch(error => console.error("songs are not able to be fetched:", error));
+//         });
+
+
+
+
 
 fetch(base_url)
     .then(res => res.json())
     .then(data => displayDetails(data))
     .catch(err => console.log(err));
+
